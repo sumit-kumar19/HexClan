4c13126b698641a18b69dd217c4a9b2210b474a9
uxharshit <harshitkatheria7890@gmail.com>
2024-05-30 09:52:39 +0530

    fixed event

--- src/components/Home/events.jsx
+++ src/components/Home/events.jsx
@@ -3,8 +3,18 @@ import { Timeline } from 'flowbite-react'
 
 export default function Events() {
     return (
-        <div className="flex justify-center items-center flex-col">
-            <Timeline className="mt-20">
+        <div className="flex justify-center items-center flex-col mt-10 mr-40 ml-40">
+            <Timeline>
+            <Timeline.Item>
+                    <Timeline.Point icon={HiCalendar} />
+                    <Timeline.Content>
+                        <Timeline.Time>2024</Timeline.Time>
+                        <Timeline.Title>Comming Soon</Timeline.Title>
+                        <Timeline.Body>
+                            More events are coming soon. Stay tuned for more updates.
+                        </Timeline.Body>
+                    </Timeline.Content>
+                </Timeline.Item>
                 <Timeline.Item>
                     <Timeline.Point icon={HiCalendar} />
                     <Timeline.Content>
@@ -12,8 +22,7 @@ export default function Events() {
                         <Timeline.Title>Code ⚡ Blaze</Timeline.Title>
                         <Timeline.Body>
                             Coding event organised with collaboration of Coding Ninjas, where more than 500 students participated.
-                            Top 10 students were awarded with goodies and certificates.
-                            
+                            Top 3 students were awarded with goodies and certificates, rest were given participation certificates.
                         </Timeline.Body>
                     </Timeline.Content>
                 </Timeline.Item>
