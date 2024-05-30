38115e155e0bf329fd19f365880c870c9a587614
uxharshit <harshitkatheria7890@gmail.com>
2024-05-30 10:26:28 +0530

    fixed animation

--- src/components/Home/carousel.jsx
+++ src/components/Home/carousel.jsx
@@ -3,7 +3,7 @@ import { Carousel } from "flowbite-react"
 export default function AnouncementCarousel() {
     return (
         <div className="h-20 sm:h-20 xl:h-20 2xl:h-20 mt-10 flex justify-center items-center">
-            <Carousel indicators={false} leftControl=" " rightControl=" " className="w-11/12" infinite={true} autoPlay={true} autoPlayInterval={5000}>
+            <Carousel indicators={false} leftControl=" " rightControl=" " className="w-11/12 bg-black rounded-2xl" infinite={true} autoPlay={true} autoPlayInterval={5000}>
                 <div className="flex h-full items-center justify-center bg-black dark:bg-white dark:text-white rounded-2xl font-serif">
                     <span className="text-4xl max-sm:text-2xl ">Welcome to Hex Clan</span>
                 </div>
